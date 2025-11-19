1333817731352d68cf1891741afd2e9fd9d90aff
KUNAL KUMAR <kunaldevspro@gmail.com>
2025-11-19 20:18:03 +0530

    feat(validation): implimented auth-middleware, cors, validation-auth

--- handlers/hander_health.go
+++ handlers/hander_health.go
@@ -2,10 +2,12 @@ package handlers
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 )
 
 func (lac *LocalApiConfig) HandlerCheckReadiness(c *gin.Context) {
+	log.Print("✓ Health Check Readiness")
 	c.JSON(http.StatusOK, gin.H{
 		"status": "Ok",
 	})
